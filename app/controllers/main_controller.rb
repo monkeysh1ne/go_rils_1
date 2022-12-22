962960bf97aa633d19adf971f7960425a76c458a
monkeys1ne <mcdoob@gmail.com>
2022-12-22 13:03:15 +1300

    Setting up session cookies for secure login

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,11 @@ class MainController < ApplicationController
 
     def index
         # the .now method on these ensures one time display in view only (does not persist in cookies)
-        flash.now[:notice] = "Logged in successfully"
-        flash.now[:alert] = "Invalid email or password"
+        # flash.now[:notice] = "Logged in successfully"
+        # flash.now[:alert] = "Invalid email or password"
+
+        if session[:user_id]
+            @user = User.find(session[:user_id])
+        end
     end
 end
